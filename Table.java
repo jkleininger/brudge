7d8db8b5480800b6357fae3558fddd5ef919c0d9
Jason Leininger <jkleininger@gmail.com>
2012-05-28 00:48:26 -0400

    0.04

--- Table.java
+++ Table.java
@@ -1,37 +1,29 @@
 public class Table {
-  AIPlayer playerSouth = new AIPlayer();
-  AIPlayer playerWest  = new AIPlayer();
-  AIPlayer playerNorth = new AIPlayer();
-  AIPlayer playerEast  = new AIPlayer();
-  Deck     theDeck     = new Deck(true);
-  Card[]   trick       = new Card[4];
-  char    suit        = 'H';
-  char    trump       = 'N';
+  AIPlayer[] player = new AIPlayer[4];
+  Deck       theDeck = new Deck(true);
+  Card[]     trick   = new Card[4];
+  char       suit    = 'H';
+  char       trump   = 'N';
 
   public Table() {
+    for(int p=0;p<4;p++) { player[p]=new AIPlayer(); }
     this.deal(0);
-    playerSouth.showHand();
-    playerWest.showHand();
-    playerNorth.showHand();
-    playerEast.showHand();
     mainPlay();
   }
 
   public void deal(int dealer) {
     for(int r=0;r<13;r++) {
-      playerSouth.dealMeOne(theDeck.dealNext());
-      playerWest.dealMeOne(theDeck.dealNext());
-      playerNorth.dealMeOne(theDeck.dealNext());
-      playerEast.dealMeOne(theDeck.dealNext());
+      for(int p=dealer+1;p<dealer+5;p++) {
+        player[p<4?p:p-4].dealMeOne(theDeck.dealNext());
+      }
     }
   }
 
   public void mainPlay() {
     for(int r=0;r<13;r++) {
-      trick[0] = playerSouth.selectAndPlay(theDeck.getPlayed(),trick,trump,suit);
-      trick[1] = playerWest.selectAndPlay(theDeck.getPlayed(), trick,trump,suit);
-      trick[2] = playerNorth.selectAndPlay(theDeck.getPlayed(),trick,trump,suit);
-      trick[3] = playerEast.selectAndPlay(theDeck.getPlayed(), trick,trump,suit);
+      for(int t=0;t<4;t++) {
+        trick[t] = player[t].selectAndPlay(theDeck.getPlayed(),trick,trump,suit);
+      }
       for(int z=0;z<4;z++) { trick[z].printCard(); System.out.print(" "); }
       System.out.println();
     }
